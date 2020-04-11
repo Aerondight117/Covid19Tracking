8b6716b14db0601d73c2ae5039c63406bc8fec60
Yauheni Hovin <4388643+Aerondight117@users.noreply.github.com>
2020-04-11 10:24:45 -0400

    working on python sql db

--- python/getOntarioData.py
+++ python/getOntarioData.py
@@ -2,6 +2,9 @@ import requests
 from contextlib import closing
 import csv
 import sqlite3
+import pandas as pd
+import pymysql
+from sshtunnel import SSHTunnelForwarder
 
 url = "https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposcovidloc.csv"
 
@@ -15,11 +18,13 @@ with open('conposcovidloc.csv', 'wb') as handle:
     for block in response.iter_content(1024):
         handle.write(block)
 
+
+
 conn = sqlite3.connect('CanadaCovidResults.db')
 c = conn.cursor()
 
 c.execute("DROP TABLE IF EXISTS `CovidCasesOntario`")
-c.execute("CREATE VIEW IF NOT EXISTS `CovidCasesCanada` AS SELECT COUNT(`ROW_ID`) AS 'NumberOfCases', Reporting_PHU,Reporting_PHU_Latitude,Reporting_PHU_Longitude FROM `CovidCasesOntario` GROUP BY `Reporting_PHU` ORDER BY COUNT(`ROW_ID`) DESC ")
+c.execute("CREATE VIEW IF NOT EXISTS `CovidCasesView` AS SELECT COUNT(`ROW_ID`) AS 'NumberOfCases', Reporting_PHU,Reporting_PHU_Latitude,Reporting_PHU_Longitude FROM `CovidCasesOntario` GROUP BY `Reporting_PHU` ORDER BY COUNT(`ROW_ID`) DESC ")
 #create a table and store the data
 
 c.execute("""CREATE TABLE `CovidCasesOntario` (
@@ -48,5 +53,37 @@ for row in reader:
     #cur.execute("CREATE INDEX location_idx ON lookup(location)" )
 
 conn.commit()
-for row in c.execute ("SELECT * FROM CovidCasesCanada"):
+
+
+sql_hostname = '127.0.0.1'
+sql_username = 'covicivy'
+sql_password = 'e#3(f$vL1MRq'
+sql_main_database = 'covicivy_covidCasesInCanada'
+sql_port = 3306
+ssh_host = 'server178.web-hosting.com'
+ssh_user = 'covicivy'
+ssh_port = 21098
+sql_ip = '127.0.0.1'
+
+with SSHTunnelForwarder(
+        (ssh_host, ssh_port),
+        ssh_username=ssh_user,
+        ssh_password= sql_password,
+        remote_bind_address=(sql_hostname, sql_port)) as tunnel:
+    conn = pymysql.connect(host='127.0.0.1', user=sql_username,
+            passwd=sql_password, db=sql_main_database,
+            port=tunnel.local_bind_port)
+    query = ''' SELECT * FROM CasesRegion'''
+    data = pd.read_sql_query(query, conn)
+    conn.close()
+
+
+
+
+
+
+for row in data: #c.execute ("SELECT * FROM CovidCasesView"):
     print(row)
+
+
+
